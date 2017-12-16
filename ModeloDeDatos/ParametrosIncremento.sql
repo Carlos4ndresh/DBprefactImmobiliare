a4633ad94d3054d75b7db82589086e117b36fbec
Carlos Herrera <carlos.herrera@outlook.com>
2017-12-16 17:27:17 -0500

    Añadidos scripts para poblar parámetros

---
+++ ModeloDeDatos/ParametrosIncremento.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM immbodb.parametros_tipoincremento
+LIMIT 0, 1000
+
+-- Date: 2017-12-16 17:26
+*/
+INSERT INTO `parametros_tipoincremento` (`id`,`tipoIncremento`,`descripcionTipoIncr`,`nombreIncrementoTipoIncremento`) VALUES (3,2,'Incremento en Meses','Meses');
+INSERT INTO `parametros_tipoincremento` (`id`,`tipoIncremento`,`descripcionTipoIncr`,`nombreIncrementoTipoIncremento`) VALUES (4,1,'Incremento en Unidades','Unidades');
