9047c7279fc82d9e8958a3de727e917d5cb6cd0c
Carlos Herrera <carlos.herrera@outlook.com>
2018-02-25 17:47:40 -0500

    cambiadas algunas relaciones, agregados scripts y modificado modelo

---
+++ ModeloDeDatos/Parametros_TipoProyecto.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM immbodb.parametros_tipoproyecto
+LIMIT 0, 1000
+
+-- Date: 2018-02-12 22:05
+*/
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (1,'VIS','Vivienda Interés Social');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (2,'VIP','Vivienda Interés Prioritario');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (3,'NO VIP','Todas las no VIS/VIP');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (4,'Comercial','Locales u Oficinas Comerciales');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (5,'Bodegas','Bodegas');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (6,'Casas','Propiedad Horizontal');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (7,'Parcelas','Parcelaciones');
+INSERT INTO `parametros_tipoproyecto` (`id`,`nombreTipoProyecto`,`descripcionTipoProyecto`) VALUES (8,'Lote','Venta de Lotes');
