e572eb20578b2780e97d0f9403d8151f596ee36e
carlosandresh <carlos.herrera@outlook.com>
2018-10-23 13:32:30 -0500

    changed user creation

--- ModeloDeDatos/ScriptBD.sql
+++ ModeloDeDatos/ScriptBD.sql
@@ -7,5 +7,5 @@ DATABASE CREATION
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `immbodb` DEFAULT CHARACTER SET utf8 ;
 USE `immbodb` ;
-CREATE USER 'factiUser' IDENTIFIED BY '85bc99cc084be156d51e11012af49e76ab1df318f9e596d8';
+CREATE USER 'factiUser' IDENTIFIED WITH mysql_native_password BY '85bc99cc084be156d51e11012af49e76ab1df318f9e596d8';
 GRANT ALL ON `immbodb`.* TO 'factiUser';
